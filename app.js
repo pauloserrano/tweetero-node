69631608b2645ac8fd754a75e69a45583edd07e7
pauloserrano <pvsserrano@gmail.com>
2022-08-25 16:51:53 -0300

    latest 10 tweets filtered

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 import express from 'express'
 import cors from 'cors'
 
-const serverUsers = []
-const latestTweets = [
+const serverUsers = [
+    {
+        username: 'bobesponja',
+        avatar: 'https://thypix.com/wp-content/uploads/2021/11/sponge-bob-profile-picture-thypix-119-408x411.jpg'
+    },
+    {
+        username: 'patrick',
+        avatar: 'https://images.unsplash.com/photo-1659535888426-de961c4ea887?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+    }, 
     {
         username: 'test',
-        avatar: 'https://images.unsplash.com/photo-1659535888426-de961c4ea887?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+        avatar: 'https://i.pinimg.com/originals/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg'
+    }
+
+]
+const latestTweets = [
+    {
+        username: 'bobesponja',
         tweet: 'lorem ipsum'
     },
     {
-        username: 'tes213t',
-        avatar: 'https://images.unsplash.com/photo-1659535888426-de961c4ea887?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+        username: 'patrick',
         tweet: 'lorem i23123psum'
     },
     {
         username: 'test',
-        avatar: 'https://images.unsplash.com/photo-1659535888426-de961c4ea887?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
         tweet: 'lorem ipsum333'
     },
 ]
@@ -31,18 +42,24 @@ app.post('/sign-up', (req, res) => {
     const user = req.body
 
     serverUsers.push(user)
-    console.log(serverUsers)
     res.send('OK!')
 })
 
 
 app.get('/tweets', (req, res) => {
-    res.send(latestTweets)
+    const tweets = latestTweets.slice(0, 10).map(tweet => {
+        return {
+            ...tweet,
+            avatar: serverUsers.find(user => user.username === tweet.username).avatar
+        }
+    })
+    res.send(tweets)
 })
 
+
 app.post('/tweets', (req, res) => {
-    const { username, tweet } = req.body
-    latestTweets.push({username, tweet})
+    const tweet = {...req.body, avatar: serverUsers.find(user => user.username === req.body.username).avatar}
+    latestTweets.unshift(tweet)
     res.send('OK')
 })
 
