11e3b251dac77362f4ec039428e50dccc0626e9e
pauloserrano <pvsserrano@gmail.com>
2022-08-26 23:35:08 -0300

    bonuses

--- app.js
+++ app.js
@@ -1,66 +1,78 @@
+import { users, tweets } from './data/mock.js'
 import express from 'express'
 import cors from 'cors'
 
-const serverUsers = [
-    {
-        username: 'bobesponja',
-        avatar: 'https://thypix.com/wp-content/uploads/2021/11/sponge-bob-profile-picture-thypix-119-408x411.jpg'
-    },
-    {
-        username: 'patrick',
-        avatar: 'https://images.unsplash.com/photo-1659535888426-de961c4ea887?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-    }, 
-    {
-        username: 'test',
-        avatar: 'https://i.pinimg.com/originals/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg'
-    }
 
-]
-const latestTweets = [
-    {
-        username: 'bobesponja',
-        tweet: 'lorem ipsum'
-    },
-    {
-        username: 'patrick',
-        tweet: 'lorem i23123psum'
-    },
-    {
-        username: 'test',
-        tweet: 'lorem ipsum333'
-    },
-]
-
-const app = express()
 const PORT = 5000
+const app = express()
+
 
 app.use(cors())
 app.use(express.json())
 
 
 app.post('/sign-up', (req, res) => {
-    const user = req.body
+    const { username, avatar } = req.body
 
-    serverUsers.push(user)
-    res.send('OK!')
-})
+    if (!username || !avatar){
+        res.status(400).send('Todos os campos são obrigatórios!')
+        return
+    }
 
-
-app.get('/tweets', (req, res) => {
-    const tweets = latestTweets.slice(0, 10).map(tweet => {
-        return {
-            ...tweet,
-            avatar: serverUsers.find(user => user.username === tweet.username).avatar
-        }
-    })
-    res.send(tweets)
+    users.push({ username, avatar })
+    res.status(201).send({message: 'OK!'})
 })
 
 
 app.post('/tweets', (req, res) => {
-    const tweet = {...req.body, avatar: serverUsers.find(user => user.username === req.body.username).avatar}
-    latestTweets.unshift(tweet)
-    res.send('OK')
+    const { user: username } = req.headers
+    const { tweet } = req.body
+
+    if (username.length <= 0 || tweet.length <= 0){
+        res.status(400).send('Todos os campos são obrigatórios!')
+        return
+    }
+
+    tweets.unshift({ username, tweet })
+    res.status(201).send({message: 'OK!'})
+})
+
+
+app.get('/tweets', (req, res) => {
+    let { page } = req.query
+    if (!page) page = 1
+    else if (page <= 0) {
+        res.status(400).send("Informe uma página válida!")
+        return
+    }
+
+    const [ pageStart, pageEnd ] = [ (page - 1) * 10 , page * 10 ]
+    const latestTweets = tweets
+        .slice(pageStart, pageEnd)
+        .map(tweet => {
+            return {
+                ...tweet,
+                avatar: users.find(user => user.username === tweet.username).avatar
+            }
+        })
+    res.send(latestTweets)
+})
+
+
+app.get('/tweets/:USERNAME', (req, res) => {
+    const { USERNAME } = req.params
+    const user = users.find(user => user.username === USERNAME)
+
+    if (!USERNAME || !user){
+        res.status(400).send('Usuário não encontrado!')
+        return
+    }
+
+    const userTweets = tweets
+        .filter(tweet => tweet.username === USERNAME)
+        .map(tweet => ({...tweet, avatar: user.avatar}))
+
+    res.send(userTweets)
 })
 
 
---
+++ data/mock.js
@@ -0,0 +1,31 @@
+const users = [
+    {
+        username: 'bobesponja',
+        avatar: 'https://thypix.com/wp-content/uploads/2021/11/sponge-bob-profile-picture-thypix-119-408x411.jpg'
+    },
+    {
+        username: 'patrick',
+        avatar: 'https://images.unsplash.com/photo-1659535888426-de961c4ea887?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+    }, 
+    {
+        username: 'test',
+        avatar: 'https://i.pinimg.com/originals/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg'
+    }
+
+]
+const tweets = [
+    {
+        username: 'bobesponja',
+        tweet: 'lorem ipsum'
+    },
+    {
+        username: 'patrick',
+        tweet: 'lorem i23123psum'
+    },
+    {
+        username: 'test',
+        tweet: 'lorem ipsum333'
+    },
+]
+
+export { users, tweets }
